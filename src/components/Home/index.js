754c4456ca84d76adcc5a84f4db95f2029da30db
cnt000 <cnt000@gmail.com>
2018-06-01 21:43:59 +0200

    add: MemoryGame, Cards component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import MemoryGame from "../MemoryGame";
 
 const Home = props => (
   <div>
     <h1>Home</h1>
     <p>Welcome home!</p>
+    <MemoryGame />
   </div>
 )
 
---
+++ src/components/MemoryGame/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Cards from "../Cards"
+
+const MemoryGame = props => (
+  <Cards />
+)
+
+export default MemoryGame
