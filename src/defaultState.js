f172a5ec018a1319e75929d8370a2a340b5157dd
cnt000 <cnt000@gmail.com>
2018-06-04 20:18:04 +0200

    add: defaultState for stories

---
+++ src/defaultState.js
@@ -0,0 +1,36 @@
+const cards = [
+  {
+    value: "A",
+    showed: false
+  },
+  {
+    value: "B",
+    showed: true
+  },
+  {
+    value: "C",
+    showed: false
+  },
+  {
+    value: "D",
+    showed: false
+  },
+  {
+    value: "A",
+    showed: false
+  },
+  {
+    value: "B",
+    showed: false
+  },
+  {
+    value: "C",
+    showed: false
+  },
+  {
+    value: "D",
+    showed: false
+  }
+]
+
+export default cards
