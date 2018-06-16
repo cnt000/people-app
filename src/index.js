bade918ae60f25e80c944b76723f75ad8bcf2d11
cnt000 <cnt000@gmail.com>
2018-06-16 13:02:54 +0200

    removed: import unused

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import store from "./store"
-import "./index.css"
 import { makeMainRoutes } from "./routes"
 import registerServiceWorker from "./registerServiceWorker"
 
