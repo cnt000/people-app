c7073885f85ef9b83cddf9fdc20cdda52fb5ed89
cnt000 <cnt000@gmail.com>
2018-06-16 18:17:18 +0200

    chore: cardClicked state

--- src/components/MemoryGame/reducer.js
+++ src/components/MemoryGame/reducer.js
@@ -9,6 +9,29 @@ import defaultState from "../../defaultState"
 // The initial state of the App
 const initialState = /*fromJS*/(defaultState);
 
+function cardClicked(state, action) {
+  let newState = {}
+  const options = state.selectedCards.length
+  
+  switch(options) {
+    case 0:
+      newState = { ...state, selectedCards: [ action.cardPosition], cards: state.cards.map((elm, i) => (i == action.cardPosition) ? { ...elm, showed: true } : elm) }
+    break;
+    case 1:
+      if(state.cards[action.cardPosition].value === state.cards[state.selectedCards[0]].value) {
+        newState = { ...state, selectedCards: [], cards: state.cards.map((elm, i) => (i == action.cardPosition) ? { ...elm, showed: true, matched: true } : elm) }
+      } else {
+        newState = { ...state, selectedCards: [], cards: state.cards.map((elm, i) => (i == state.selectedCards[0]) ? { ...elm, showed: false } : elm) }
+      }
+    break;
+    default:
+      newState = { ...state }
+  }
+
+  return newState;
+
+}
+
 function appReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_CARDS:
@@ -16,7 +39,7 @@ function appReducer(state = initialState, action) {
         //.set('loading', false)
         //.set('error', false)
     case SHOW_CARD:
-      return { ...state, cards: state.cards.map((elm, i) => (i == action.cardPosition) ? { ...elm, showed: true } : elm) }
+      return cardClicked(state, action)
     default:
       return state;
   }
