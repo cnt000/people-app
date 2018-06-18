4b860d11e203ab78318e21eb69a0acf567fb3b54
cnt000 <cnt000@gmail.com>
2018-06-19 00:06:51 +0200

    add: enzyme and conf

--- src/__tests__/components/Card/index.test.js
+++ src/__tests__/components/Card/index.test.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from '../../../components/Card';
+import { shallow } from 'enzyme';
 
-it('Card renders hidden', () => {
+it('Card render', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Card value={"A"} showed={false} onClick={() => console.log('click')} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('Card renders showed', () => {
+it('Card render showed', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Card value={"A"} showed={true} onClick={() => console.log('click')} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
+
+// it('Card renders showed', () => {
+//   shallow(<Card value={"A"} showed={true} onClick={() => console.log('click')} />);
+// });
