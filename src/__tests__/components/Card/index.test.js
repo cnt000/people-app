5973a9c85de4795e62441f987082a94e5de493f0
cnt000 <cnt000@gmail.com>
2018-06-28 18:00:48 +0200

    add: test files for components and snapshots

--- src/__tests__/components/Card/index.test.js
+++ src/__tests__/components/Card/index.test.js
@@ -2,20 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from '../../../components/Card';
 import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
 
-it('Card render', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card value={"A"} showed={false} onClick={() => console.log('click')} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Card', () => {
+  it('should render (without snapshot)', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card value={"A"} showed={false} onClick={() => console.log('click')} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should render value if showed', () => {
+    const wrapper = shallow(<Card value={'D'} showed={true} onClick={() => console.log('click')} />);
+    const value = 'D';
+    expect(wrapper.contains(value)).toEqual(true);
+  });
+
+  it('should render correctly showed', () => {
+    const output = shallow(
+      <Card value={"A"} showed={true} onClick={() => console.log('click')} />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('fire an onClick', () => {
+    const output = shallow(
+      <Card value={"A"} showed={true} onClick={() => alert('click')} />
+    );
+    output.simulate('click');
+    expect(window.alert).toHaveBeenCalledWith('click');
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should render correctly hided', () => {
+    const output = shallow(
+      <Card value={"B"} showed={false} onClick={() => console.log('click')} />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
 });
-
-it('Card render showed', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card value={"A"} showed={true} onClick={() => console.log('click')} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-
-// it('Card renders showed', () => {
-//   shallow(<Card value={"A"} showed={true} onClick={() => console.log('click')} />);
-// });
---
+++ src/__tests__/components/Cards/index.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cards from '../../../components/Cards';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import defaulState from '../../../defaultState'
+
+describe('Cards', () => {
+  it('should render (without snapshot)', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Cards cards={defaulState.cards} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // it('should render a <ul>', () => {
+  //   const wrapper = shallow(<Cards cards={[{
+  //     value: "DDD",
+  //     showed: false
+  //   }]} />);
+  //   const value = <ul></ul>;
+  //   expect(wrapper.contains(value)).toEqual(true);
+  // });
+
+  it('should render correctly empty', () => {
+    const output = shallow(
+      <Cards cards={[]} />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should render correctly with snapshot', () => {
+    const output = shallow(
+      <Cards cards={defaulState.cards} />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
