e6df204af7b6e59452dd81b397f55765f0ad1deb
cnt000 <cnt000@gmail.com>
2018-07-12 22:43:09 +0200

    add: more props for cards component test

--- src/__tests__/components/MemoryGame/cards.test.js
+++ src/__tests__/components/MemoryGame/cards.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Cards from '../../../components/MemoryGame/cards'
-import ButtonStart from '../../../components/MemoryGame/button-start'
 import { shallow, mount } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
 import defaulState from '../../../defaultState'
@@ -9,39 +8,115 @@ import defaulState from '../../../defaultState'
 describe('Cards', () => {
   it('should render (without snapshot)', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Cards cards={defaulState.cards} />, div)
+    ReactDOM.render(
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={false}
+        isClickable={false}
+        hasWin={false}
+      />,
+      div
+    )
     ReactDOM.unmountComponentAtNode(div)
   })
 
   it('should render before playing', () => {
     const output = shallow(
-      <Cards cards={defaulState.cards} isPlaying={false} />
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={false}
+        isClickable={false}
+        hasWin={false}
+      />
     )
     expect(shallowToJson(output)).toMatchSnapshot()
   })
 
   it('should render playing', () => {
-    const output = shallow(<Cards cards={defaulState.cards} isPlaying={true} />)
+    const output = shallow(
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={true}
+        isClickable={false}
+        hasWin={false}
+      />
+    )
     expect(shallowToJson(output)).toMatchSnapshot()
   })
 
   it('should render after playing (finished)', () => {
-    const output = shallow(<Cards cards={defaulState.cards} hasWin={true} />)
+    const output = shallow(
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={false}
+        isClickable={false}
+        hasWin={true}
+      />
+    )
     expect(shallowToJson(output)).toMatchSnapshot()
   })
 
   it('should render "YOU WIN" when finished', () => {
-    const output = mount(<Cards cards={defaulState.cards} hasWin={true} />)
+    const output = mount(
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={false}
+        isClickable={false}
+        hasWin={true}
+      />
+    )
     expect(output.text()).toContain('HAI VINTO')
   })
 
   it('should render correctly empty', () => {
-    const output = shallow(<Cards cards={[]} />)
+    const output = shallow(
+      <Cards
+        cards={[]}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={false}
+        isClickable={false}
+        hasWin={false}
+      />
+    )
     expect(shallowToJson(output)).toMatchSnapshot()
   })
 
   it('should render correctly with snapshot', () => {
-    const output = shallow(<Cards cards={defaulState.cards} />)
+    const output = shallow(
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={true}
+        isClickable={true}
+        hasWin={false}
+      />
+    )
+    expect(shallowToJson(output)).toMatchSnapshot()
+  })
+
+  it('should render correctly with snapshot, isClickable false', () => {
+    const output = shallow(
+      <Cards
+        cards={defaulState.cards}
+        showCard={() => console.log('click show card')}
+        startGame={() => console.log('click start game')}
+        isPlaying={true}
+        isClickable={false}
+        hasWin={false}
+      />
+    )
     expect(shallowToJson(output)).toMatchSnapshot()
   })
 })
