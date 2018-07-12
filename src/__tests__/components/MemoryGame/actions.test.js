0527d9bd946eb875d365a5466a37da73fa04c59f
cnt000 <cnt000@gmail.com>
2018-07-12 22:43:39 +0200

    add: testing actions

---
+++ src/__tests__/components/MemoryGame/actions.test.js
@@ -0,0 +1,43 @@
+import {
+  LOAD_CARDS,
+  SHOW_CARD,
+  CHECK_COUPLE,
+  START_GAME,
+} from '../../../components/MemoryGame/constants'
+import {
+  loadCards,
+  showCard,
+  checkCouple,
+  startGame,
+} from '../../../components/MemoryGame/actions'
+
+describe('actions', () => {
+  it('should create an action to loadCards', () => {
+    const expectedAction = {
+      type: LOAD_CARDS,
+    }
+    expect(loadCards()).toEqual(expectedAction)
+  })
+
+  it('should create an action to show a card', () => {
+    const expectedAction = {
+      type: SHOW_CARD,
+      cardPosition: 3,
+    }
+    expect(showCard(3)).toEqual(expectedAction)
+  })
+
+  it('should create an action to start game', () => {
+    const expectedAction = {
+      type: START_GAME,
+    }
+    expect(startGame()).toEqual(expectedAction)
+  })
+
+  it('should create an action to check for a couple', () => {
+    const expectedAction = {
+      type: CHECK_COUPLE,
+    }
+    expect(checkCouple()).toEqual(expectedAction)
+  })
+})
