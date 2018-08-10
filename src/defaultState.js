3a1dc4bff2fb3b876b5d7d3e49a76840765fdc3b
cnt000 <cnt000@gmail.com>
2018-08-10 19:50:29 +0200

    add: shuffleArray in Helpers

--- src/defaultState.js
+++ src/defaultState.js
@@ -1,16 +1,5 @@
 const cards = ['AAA', 'BBB', 'AAA', 'DDD', 'BBB', 'DDD']
 
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1))
-    var temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-  return array
-}
-
 const gameState = {
   selectedCards: [],
   cards: cards.map(val => ({ value: val, showed: false })),
---
+++ src/modules/Helpers/index.js
@@ -0,0 +1,18 @@
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+export function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
+
+export function timerGame(callback) {
+  console.log('Ready....go!')
+  setTimeout(() => {
+    console.log('Times up -- stop!')
+    callback && callback()
+  }, 1000)
+}
